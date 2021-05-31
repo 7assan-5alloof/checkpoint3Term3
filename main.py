79d9200effbb38a609315462ff441e16a0509c8a
7assan-5alloof <hassankhalloof20@gmail.com>
2021-05-31 09:55:08 +0400

    Part B

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ periodicTable = []
 for i in range(4):
     periodicTable.append(input("Enter an element: "))
 print(periodicTable)
+
+# Part B
+del(periodicTable[1])
+for i in range(2):
+    periodicTable.append(input("Enter an element: "))
+print(periodicTable)
+print(periodicTable[0])
+print(periodicTable[2])
+print(len(periodicTable))
